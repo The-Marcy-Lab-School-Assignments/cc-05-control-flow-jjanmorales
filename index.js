e378d33aa4dabb011c3a7a629efa652ff37c1ec3
Jan Morales <moralesjan40@gmail.com>
2021-10-06 14:13:20 +0000

    Completed two problems

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 //1
-function absoluteValue(){
-  
+function absoluteValue(num){
+  if(num < 0){
+      console.log(num *= -1)
+  }else if(num > 0){
+      console.log(num)
+  }else{
+      return null
+  }
 }
 
+console.log(absoluteValue("tee"))
 
+function multiplesOfFourAndSix() {
+    let array = [];
+    for(let i = 1; i <= 100; i++){
+        if(i % 4 === 0 && i % 6 === 0){
+            array.push(i)
+        }
+    }return array
+}
 
+console.log(multiplesOfFourAndSix());
 
